53f022719a00220fcb467b41df0f1549df80c5bd
Zennerharauf <163380432+Zennerharauf@users.noreply.github.com>
2024-04-16 03:31:30 +0500

    Add files via upload

---
+++ Project00 - Simple Command Line Calculator/index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env/ node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { massage: "Enter first number", type: "number", name: "firstNumber" },
+    { massage: "Enter second number", type: "number", name: "secondNumber" },
+    { massage: "Select one of the operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Divison"]
+    }
+]);
+//conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Divison") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please select vaild operator");
+}
